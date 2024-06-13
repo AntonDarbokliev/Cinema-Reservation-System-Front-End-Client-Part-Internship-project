1d3928b3aef2641b6a37499d66c90ef8a8313108
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-06-13 18:55:32 +0300

    adjustments

--- Cinema-Reservation-System-Front-End-Client-Part-Internship-project/src/app/app.component.ts
+++ Cinema-Reservation-System-Front-End-Client-Part-Internship-project/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainNavigationComponent } from './navigation/main-navigation/main-navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainNavigationComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Cinema-Reservation-System-Front-End-Client-Part-Internship-project';
