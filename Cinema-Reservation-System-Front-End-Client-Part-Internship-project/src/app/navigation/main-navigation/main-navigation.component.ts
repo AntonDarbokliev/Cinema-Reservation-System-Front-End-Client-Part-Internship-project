2ffae347fae14cb068a940b28c1e55e34c3787b8
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-06-14 20:06:30 +0300

    remove unnecessary  css

--- Cinema-Reservation-System-Front-End-Client-Part-Internship-project/src/app/navigation/main-navigation/main-navigation.component.ts
+++ Cinema-Reservation-System-Front-End-Client-Part-Internship-project/src/app/navigation/main-navigation/main-navigation.component.ts
@@ -6,6 +6,5 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [RouterLink],
   templateUrl: './main-navigation.component.html',
-  styleUrl: './main-navigation.component.css',
 })
 export class MainNavigationComponent {}
