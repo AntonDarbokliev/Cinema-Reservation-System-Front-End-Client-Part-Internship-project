a2cf1cd910921ae544917a67fe97f59c5e51b06b
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-06-14 22:50:00 +0300

    share user state accross app

--- Cinema-Reservation-System-Front-End-Client-Part-Internship-project/src/app/authentication/shared/authentication.service.ts
+++ Cinema-Reservation-System-Front-End-Client-Part-Internship-project/src/app/authentication/shared/authentication.service.ts
@@ -3,23 +3,52 @@ import { Injectable } from '@angular/core';
 import { CreateUser } from './create-user.model';
 import { environment } from '../../../environments/environment';
 import { LoginUser } from './login-user.model';
+import { BehaviorSubject, tap } from 'rxjs';
+import { User } from './user-model';
+import { jwtDecode } from 'jwt-decode';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationService {
-  constructor(private http: HttpClient) {}
-
   private apiUrl = environment.apiUrl;
+  private userSubject = new BehaviorSubject<User | null>(null);
+  public user = this.userSubject.asObservable();
+
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+  ) {
+    const savedToken = localStorage.getItem('token');
+    if (savedToken) {
+      const user: User = jwtDecode(savedToken);
+      this.userSubject.next(user);
+    }
+  }
 
   register(createUser: CreateUser) {
-    return this.http.post(`${this.apiUrl}/auth/register`, createUser);
+    return this.http.post(`${this.apiUrl}/auth/register`, createUser).pipe(
+      tap(() => {
+        this.router.navigate(['/login']);
+      }),
+    );
   }
 
   login(loginUser: LoginUser) {
-    return this.http.post<{ access_token: string }>(
-      `${this.apiUrl}/auth/login`,
-      loginUser,
-    );
+    return this.http
+      .post<{ access_token: string }>(`${this.apiUrl}/auth/login`, loginUser)
+      .pipe(
+        tap((response) => {
+          const user: User = jwtDecode(response.access_token);
+          localStorage.setItem('token', response.access_token);
+          this.userSubject.next(user);
+        }),
+      );
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.userSubject.next(null);
   }
 }
--- Cinema-Reservation-System-Front-End-Client-Part-Internship-project/src/app/navigation/main-navigation/main-navigation.component.ts
+++ Cinema-Reservation-System-Front-End-Client-Part-Internship-project/src/app/navigation/main-navigation/main-navigation.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { InputFieldComponent } from '../../shared/input-field/input-field.component';
+import { Subscription } from 'rxjs';
+import { AuthenticationService } from '../../authentication/shared/authentication.service';
+import { User } from '../../authentication/shared/user-model';
 
 @Component({
   selector: 'app-main-navigation',
@@ -8,4 +11,14 @@ import { InputFieldComponent } from '../../shared/input-field/input-field.compon
   imports: [RouterLink, InputFieldComponent],
   templateUrl: './main-navigation.component.html',
 })
-export class MainNavigationComponent {}
+export class MainNavigationComponent {
+  user: User | null = null;
+
+  constructor(private authService: AuthenticationService) {}
+
+  ngOnInit(): void {
+    this.authService.user.subscribe((user) => {
+      this.user = user;
+    });
+  }
+}
