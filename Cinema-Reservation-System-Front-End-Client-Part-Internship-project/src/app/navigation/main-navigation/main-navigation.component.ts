fc1a0ce47a831ea4b5e75e5778c19f26891e9a0d
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-06-14 21:01:42 +0300

    Navigation styling

--- Cinema-Reservation-System-Front-End-Client-Part-Internship-project/src/app/navigation/main-navigation/main-navigation.component.ts
+++ Cinema-Reservation-System-Front-End-Client-Part-Internship-project/src/app/navigation/main-navigation/main-navigation.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { InputFieldComponent } from '../../shared/input-field/input-field.component';
 
 @Component({
   selector: 'app-main-navigation',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, InputFieldComponent],
   templateUrl: './main-navigation.component.html',
 })
 export class MainNavigationComponent {}
