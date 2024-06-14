cfb8c67d6f33bd71a0b4f9bfa1c474c100766bb6
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-06-14 18:29:55 +0300

    provide class interceptor

--- Cinema-Reservation-System-Front-End-Client-Part-Internship-project/src/app/app.config.ts
+++ Cinema-Reservation-System-Front-End-Client-Part-Internship-project/src/app/app.config.ts
@@ -2,12 +2,22 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { HttpErrorInterceptor } from './shared/http-error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorInterceptor,
+      multi: true,
+    },
   ],
 };
