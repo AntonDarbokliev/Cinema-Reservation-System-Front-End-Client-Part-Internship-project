3c57703ae3b365a7f3e6d00827c3e475078a1fab
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-06-14 20:05:49 +0300

    login component

--- Cinema-Reservation-System-Front-End-Client-Part-Internship-project/src/app/authentication/login/login.component.ts
+++ Cinema-Reservation-System-Front-End-Client-Part-Internship-project/src/app/authentication/login/login.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InputFieldComponent } from '../../shared/input-field/input-field.component';
+import { ButtonComponent } from '../../shared/button/button.component';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthenticationService } from '../shared/authentication.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [InputFieldComponent, ButtonComponent, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({});
+  formSubmitted = false;
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthenticationService,
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(5)]),
+      ],
+    });
+  }
+
+  onSubmit() {
+    this.formSubmitted = true;
+    if (this.loginForm.valid) {
+      this.authService
+        .login(this.loginForm.value)
+        .subscribe((data: { access_token: string }) => {
+          localStorage.setItem('token', data['access_token']);
+          
+        });
+    }
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
 }
