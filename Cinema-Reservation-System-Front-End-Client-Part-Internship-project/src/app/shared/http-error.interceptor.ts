42be0e53eab873d205795584f8996fcd34303db5
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-06-14 18:29:30 +0300

    http error interceptor

---
+++ Cinema-Reservation-System-Front-End-Client-Part-Internship-project/src/app/shared/http-error.interceptor.ts
@@ -0,0 +1,34 @@
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { ToastService } from '../toasts/toast.service';
+import { ErrorObj } from './error-obj.model';
+
+@Injectable()
+export class HttpErrorInterceptor implements HttpInterceptor {
+  constructor(private toastService: ToastService) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler,
+  ): Observable<HttpEvent<unknown>> {
+    console.log('ERROR INTERCEPTOR CALLED');
+
+    return next.handle(request).pipe(
+      catchError((err: HttpErrorResponse) => {
+        const errorObj: ErrorObj = err.error;
+        errorObj.message.forEach((msg) => {
+          this.toastService.addToast({ message: msg, type: 'error' });
+        });
+
+        return throwError(() => err);
+      }),
+    );
+  }
+}
