827dd9136f6b380f7d16ff4c4e28dfbd767de357
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-06-14 20:06:15 +0300

    login service/model

--- Cinema-Reservation-System-Front-End-Client-Part-Internship-project/src/app/authentication/shared/authentication.service.ts
+++ Cinema-Reservation-System-Front-End-Client-Part-Internship-project/src/app/authentication/shared/authentication.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CreateUser } from './create-user.model';
 import { environment } from '../../../environments/environment';
+import { LoginUser } from './login-user.model';
 
 @Injectable({
   providedIn: 'root',
@@ -13,5 +14,12 @@ export class AuthenticationService {
 
   register(createUser: CreateUser) {
     return this.http.post(`${this.apiUrl}/auth/register`, createUser);
+  }
+
+  login(loginUser: LoginUser) {
+    return this.http.post<{ access_token: string }>(
+      `${this.apiUrl}/auth/login`,
+      loginUser,
+    );
   }
 }
