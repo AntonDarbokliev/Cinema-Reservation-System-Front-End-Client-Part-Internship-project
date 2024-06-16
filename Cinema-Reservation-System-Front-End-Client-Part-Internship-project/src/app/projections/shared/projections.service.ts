4ad614319ac255bb7706cdcf06f71b93351d9879
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-06-16 16:49:20 +0300

    dynamically fetch cinema's movies

--- Cinema-Reservation-System-Front-End-Client-Part-Internship-project/src/app/projections/shared/projections.service.ts
+++ Cinema-Reservation-System-Front-End-Client-Part-Internship-project/src/app/projections/shared/projections.service.ts
@@ -2,16 +2,22 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Movie } from '../../shared/models/movie.model';
+import { CinemaService } from '../../shared/services/cinema.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProjectionService {
   private apiUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private cinemaService: CinemaService,
+  ) {}
 
   getMoviesWithProjections() {
-    return this.http.get<Movie[]>(`${this.apiUrl}/movies/cinema/${'664cedffce82675ad95c08e4'}?projections=true`); // Change static cinema id later
+    return this.http.get<Movie[]>(
+      `${this.apiUrl}/movies/cinema/${this.cinemaService.cinema.getValue()._id}?projections=true`,
+    );
   }
 }
