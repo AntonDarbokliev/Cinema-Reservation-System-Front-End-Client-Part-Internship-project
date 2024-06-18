b62e53db3927798972236c74756c5b63ed5c8492
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-06-18 17:57:09 +0300

    row component

---
+++ Cinema-Reservation-System-Front-End-Client-Part-Internship-project/src/app/reservations/hall-layout/components/row/row.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Row } from '../../../../shared/models/hall.model';
+import { SeatComponent } from '../seat/seat.component';
+
+@Component({
+  selector: 'app-row',
+  standalone: true,
+  imports: [SeatComponent],
+  templateUrl: './row.component.html',
+  styleUrl: './row.component.css',
+})
+export class RowComponent {
+  @Input() row: Row = { _id: '', seats: [] };
+}
