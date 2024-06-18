677c605430bad9ea64300e8e63a947914cc385b3
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-06-18 21:58:03 +0300

    add stages to reserving/buying a ticket, initialize reservation service variables

--- Cinema-Reservation-System-Front-End-Client-Part-Internship-project/src/app/reservations/reservation-page/reservation-page.component.ts
+++ Cinema-Reservation-System-Front-End-Client-Part-Internship-project/src/app/reservations/reservation-page/reservation-page.component.ts
@@ -1,26 +1,45 @@
-import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProjectionService } from '../../projections/shared/projections.service';
 import { Projection } from '../../shared/models/projection.model';
 import { Movie } from '../../shared/models/movie.model';
 import { MovieService } from '../../shared/services/movie.service';
+import { HallLayoutComponent } from '../hall-layout/hall-layout.component';
+import { ReservationsService } from '../shared/reservations.service';
+import { ButtonComponent } from '../../shared/components/button/button.component';
+
+enum ReservationStage {
+  'SEAT_SELECTION' = 0,
+  'SIDES_SELECTION' = 1,
+  'SUMMARY' = 2,
+}
 
 @Component({
   selector: 'app-reservation-page',
   standalone: true,
-  imports: [],
+  imports: [HallLayoutComponent, ButtonComponent],
   templateUrl: './reservation-page.component.html',
   styleUrl: './reservation-page.component.css',
 })
-export class ReservationPageComponent implements OnInit {
+export class ReservationPageComponent implements OnInit, OnDestroy {
   projectionId: string | null = '';
   projection: Projection | null = null;
   movie: Movie | null = null;
+  stage: ReservationStage = ReservationStage.SEAT_SELECTION;
+  enum: typeof ReservationStage = ReservationStage;
+  showNextButton: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
     private projectionService: ProjectionService,
     private movieService: MovieService,
+    private reservationsService: ReservationsService,
   ) {
     this.projectionId = this.route.snapshot.paramMap.get('projectionId');
   }
@@ -32,13 +51,32 @@ export class ReservationPageComponent implements OnInit {
         .subscribe((projection) => {
           this.projection = projection;
           this.initializeMovie(projection);
+          this.initializeReservations(projection);
+          this.initializeTickets(projection);
         });
+      this.reservationsService.selectedSeat.subscribe((selectedSeat) => {
+        if (selectedSeat) {
+          this.showNextButton = true;
+        }
+      });
     }
+  }
+
+  ngOnDestroy(): void {
+    this.reservationsService.setSelectedSeat(null);
   }
 
   private initializeMovie(projection: Projection) {
     this.movieService.getMovie(projection.movieId).subscribe((movie) => {
       this.movie = movie;
     });
+  }
+
+  private initializeReservations(projection: Projection) {
+    this.reservationsService.setReservations(projection.reservations);
+  }
+
+  private initializeTickets(projection: Projection) {
+    this.reservationsService.setTickets(projection.tickets);
   }
 }
